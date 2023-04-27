1dcdef72e37aebf88445d419e257ae6d189d3fe9
TLP-269 <a>
2023-04-27 20:15:04 +0300

    Initial commit

---
+++ basic_SIR.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def f(SIR):
+    return np.array([])
+
+def run_simulation((S,I,R) = np.array([9e6 - 10, 10, 0]), N = 9e6, gama = 1/4, beta = 1/2):
